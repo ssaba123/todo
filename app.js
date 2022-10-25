1a05e7d0560583300c1e292e521aed250d7115dc
sabashaikh1 <ss.sabashaikh1@gmail.com>
2022-10-25 17:21:03 -0400

    Add items to list

--- app.js
+++ app.js
@@ -2,16 +2,18 @@ const list = document.querySelector('.list-wrapper');
 const toDoBtn = document.querySelector('.todo-btn');
 const toDo = document.querySelector('.todo');
 
-
 toDoBtn.addEventListener('click', addToDo);
+toDo.addEventListener('change', newToDo)
 
-toDo.addEventListener('change', function(e){
-    newItem.textContent = e.target.value;
-})
+function newToDo(e){
+toDo.textContent = e.target.value
+}
 
 function addToDo(){
     const newItem = document.createElement('li');
+    newItem.textContent = toDo.textContent;
     list.appendChild(newItem);
+    toDo.textContent = '';
 }
 
 
