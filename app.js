dd8207634937162de9f02dce8bffbf6eb52109d3
sabashaikh1 <ss.sabashaikh1@gmail.com>
2022-10-25 17:46:36 -0400

    Add styling

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const list = document.querySelector('.list-wrapper');
 const toDoBtn = document.querySelector('.todo-btn');
-const toDo = document.querySelector('.todo');
+const toDo = document.querySelector('.todo-input');
 
 toDoBtn.addEventListener('click', addToDo);
 toDo.addEventListener('change', newToDo)
@@ -11,6 +11,7 @@ toDo.textContent = e.target.value
 
 function addToDo(){
     const newItem = document.createElement('li');
+    newItem.classList.add('task');
     newItem.textContent = toDo.textContent;
     list.appendChild(newItem);
     toDo.textContent = '';
