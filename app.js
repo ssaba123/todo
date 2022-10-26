ef255dbd561a0e7d735a0252053b32d7eae86263
sabashaikh1 <ss.sabashaikh1@gmail.com>
2022-10-26 10:21:32 -0400

    Remove assets

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 const list = document.querySelector('.list-wrapper');
 const toDoBtn = document.querySelector('.todo-btn');
 const toDo = document.querySelector('.todo-input');
+const darkMode = document.querySelector('.dark-mode')
+const body = document.body
+
+darkMode.addEventListener('click', function turnDark(){
+   body.classList.toggle('dark')
+})
 
 toDoBtn.addEventListener('click', addToDo);
 toDo.addEventListener('change', newToDo)
@@ -10,11 +16,16 @@ toDo.textContent = e.target.value
 }
 
 function addToDo(){
-    const newItem = document.createElement('li');
-    newItem.classList.add('task');
-    newItem.textContent = toDo.textContent;
-    list.appendChild(newItem);
-    toDo.value = '';
+    if(toDo.textContent.length > 1){
+        const newItem = document.createElement('li');
+        newItem.classList.add('task');
+        newItem.textContent = toDo.textContent;
+        list.appendChild(newItem);
+        toDo.value = '';
+        toDo.textContent = '';
+    } else {
+        alert('Task cannot be empty!')
+    }   
 }
 
 
